d3e80da6aace0086d1ccc82ac41364fc9af6bb47
Vladislav Murzin <vlad99samara@mail.ru>
2022-04-15 13:02:48 +0400

    add php

--- src/js/script.js
+++ src/js/script.js
@@ -55,3 +55,69 @@
 
 	toggleSlide('.catalog-item__link');
 	toggleSlide('.catalog-item__back');
+
+	//Modal
+
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn();
+	});
+	$('.modal__close').on('click', function() {
+		$('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+	});
+
+
+	$('.button_mini').each(function(i) {
+		$(this).on('click', function() {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn();
+		})
+	});
+
+	//Forms
+
+	function validForms(form){
+		$(form).validate({
+			rules: {
+				name: "required",
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: "Пожалуйста, введите ваше имя",
+				phone: "Пожалуйста, введите ваш номер телефона",
+				email: {
+					required: "Пожалуйста, введите ваш email",
+					email: "Неправильный формат email"
+				}
+			}
+		});
+	};
+
+	validForms('#consultation-form');
+	validForms('#consultation form');
+	validForms('#order form');
+
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+
+		if (!$(this).valid()) {
+			return;
+		}
+		$.ajax ({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+
+			$('form').trigger('reset');
+		});
+		return false;
+	});
